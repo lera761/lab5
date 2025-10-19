d5cd9e1b02eeb3e50ced5ddbd102751655849552
lera761 <lera21102008@gmail.com>
2025-10-19 20:02:59 +0300

    Add files via upload

---
+++ lab5 wile/lab5 wile/Program.cs
@@ -0,0 +1,35 @@
+﻿int a, b, c, d, s;
+s = 0;
+
+Console.WriteLine("введите первый промежуток");
+Console.WriteLine("введите от");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите до");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второй промежуток");
+Console.WriteLine("введите От");
+c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите До");
+d = Convert.ToInt32(Console.ReadLine());
+
+if (a <= b && b <= c && c <= d)
+{
+    int i = c;
+    while ( i <= d)
+    {
+
+        if (i % 2 == 0)
+        {
+            s += i;
+        }
+        i++;
+    }
+    Console.WriteLine("Сумма четных чисел: " + s);
+}
+else
+{
+    Console.WriteLine("Не соответствует условию a ≤ b ≤ c ≤ d");
+}
