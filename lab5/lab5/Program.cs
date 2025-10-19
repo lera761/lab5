1ecdc7724faa7700f012d378f8521da378d1a6ae
lera761 <lera21102008@gmail.com>
2025-10-19 20:13:39 +0300

    Add files via upload

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,32 @@
+﻿int a, b, c, d, s;
+s = 0;
+
+Console.WriteLine("введите первый промежуток");
+Console.WriteLine("введите от");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите до");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второй промежуток");
+Console.WriteLine("введите От");
+c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите До");
+d = Convert.ToInt32(Console.ReadLine());
+
+if (a <= b && b <= c && c <= d)
+{
+    for (int i = c; i <= d; i++) 
+    {
+    if (i % 2 == 0)
+    {
+        s += i;
+    }
+}
+Console.WriteLine("Сумма четных чисел: " + s);
+}
+else
+{
+    Console.WriteLine("Не соответствует условию a ≤ b ≤ c ≤ d");
+}
